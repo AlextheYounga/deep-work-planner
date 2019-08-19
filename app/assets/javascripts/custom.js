a917422fb82f4327c4a4d13929e2be676095834d
alex <alex@izoox.com>
2019-08-18 18:50:32 -0600

    Creating interactive buttons

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,1 +1,14 @@
 var current_time = moment().format('h:mm a');
+
+$('.plus-button').hover(function () {
+    $('.button-clarify').removeClass('hidden');
+    $('.button-clarify').addClass('inline-block');
+    $('.add-new-button').addClass('rounded-lg');
+    $('.add-new-button').addClass('bg-blue-400');
+
+}, function () {
+    $('.button-clarify').removeClass('inline-block');
+    $('.button-clarify').addClass('hidden');
+    $('.add-new-button').removeClass('rounded-lg');
+    $('.add-new-button').removeClass('bg-blue-400');
+});
