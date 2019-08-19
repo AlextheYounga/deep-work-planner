ec8e768f9663b46446d10388e3e6360092a85c30
alex <alex@izoox.com>
2019-08-18 20:06:40 -0600

    Button styling looking good

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,14 +1,10 @@
 var current_time = moment().format('h:mm a');
 
-$('.plus-button').hover(function () {
-    $('.button-clarify').removeClass('hidden');
-    $('.button-clarify').addClass('inline-block');
-    $('.add-new-button').addClass('rounded-lg');
-    $('.add-new-button').addClass('bg-blue-400');
+// $('.plus-button').hover(function () {
+//     $('.button-clarify').removeClass('hidden');
+//     $('.button-clarify').addClass('inline-block');
 
-}, function () {
-    $('.button-clarify').removeClass('inline-block');
-    $('.button-clarify').addClass('hidden');
-    $('.add-new-button').removeClass('rounded-lg');
-    $('.add-new-button').removeClass('bg-blue-400');
-});
+// }, function () {
+//     $('.button-clarify').removeClass('inline-block');
+//     $('.button-clarify').addClass('hidden');
+// });
