579e886de5d9d4529700fb4e663890814d95f37f
alex <alex@izoox.com>
2019-08-19 22:14:08 -0600

    Creating mobile button

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,10 +1,24 @@
 var current_time = moment().format('h:mm a');
 
-// $('.plus-button').hover(function () {
-//     $('.button-clarify').removeClass('hidden');
-//     $('.button-clarify').addClass('inline-block');
+var time_block = '<div class="time-block border-blue-400 border-l-2 my-12">' +
+    '<div class="pl-2">' +
+    '<input type="text" placeholder="XX:XX PM" class="time-start font-thin" />' +
+    '</div>' +
+    '<div class="pl-4 py-4">' +
+    '<textarea id="" placeholder="Task at hand" rows="3" class="task helvetica italic p-2 tracking-wide font-light text-gray-900"></textarea>' +
+    '</div>' +
+    '<div class="pl-2">' +
+    '<input type="text" placeholder="XX:XX PM" class="font-thin" />' +
+    '</div>' +
+    '</div>';
 
-// }, function () {
-//     $('.button-clarify').removeClass('inline-block');
-//     $('.button-clarify').addClass('hidden');
-// });
+var time_block_button = '<div class="add-time-block flex ml-8 mt-8">' +
+    '<button class="time-block-button bg-gray-400 h-8 relative w-8"></button>' +
+    '<span class="time-block-text font-light helvetica italic ml-2 pt-1 text-white tracking-wide w-40">Add New Time Block</span>' +
+    '</div>';
+
+$('.time-block-button').click(function () {
+    $('.add-time-block').remove();
+    $('.time-column').append(time_block);
+    $('.time-column').append(time_block_button);
+});
