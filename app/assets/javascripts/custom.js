53fb6bd5bcb621f2a37cfce991d9b8adb87fea74
alex <alex@izoox.com>
2019-08-26 22:05:54 -0600

    Cleaning up

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,14 +1,72 @@
-var time_block_button = '<div class="add-time-block flex ml-8 mt-8">' +
-    '<button class="bg-gray-400 h-8 relative w-8" id="time-block-button"></button>' +
-    '<span class="time-block-text font-light helvetica italic ml-2 pt-1 text-white tracking-wide w-40">Add New Time Block</span>' +
-    '</div>';
 var current_time = moment().format('h:mm a');
 var am_pm = moment().format('a');
-// var time_block_end = $('body input[name="time-last"]').last().val();
-// var time_block_start = $('body input[name="time-start"]').last().val();
-
 
 $(document).ready(function () {
+    //Math functions for time and automatically inserting colon
+    $('#time-start , #time-last').keydown(function (e) {
+        if (e.ctrlKey || e.metaKey) {
+            return true;
+        }
+
+        if (e.which >= 37 && e.which <= 40) {
+            return true;
+        }
+
+        if (e.which !== 8 && e.which !== 0 && e.key.match(/[^:0-9]/)) {
+            return false;
+        }
+    }).keyup(function (e) {
+        var $this = $(this);
+
+        if (e.ctrlKey || e.metaKey || e.which === 8 || e.which === 0 || (e.which >= 37 && e.which <= 40)) {
+            return true;
+        }
+
+        var ss = parseInt(this.selectionStart);
+
+        var val = $this.val();
+        var t = val.replace(/[^0-9]/g, '');
+        var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
+        var m = Math.max(0, Math.min(59, parseInt(t.substr(2))));
+
+        if (t.length < 3) {
+            m = '';
+        }
+
+        var r;
+
+        if (val.length === 2) {
+            r = String('0' + h).substr(String(h).length - 1) + ':';
+            ss++;
+        } else if (val.length >= 3 && val.length < 5) {
+            r = String('0' + h).substr(String(h).length - 1) + ':' + m;
+            ss++;
+        } else if (val.length === 5) {
+            r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
+        }
+
+        if (r && r !== $this.val()) {
+            $this.val(r);
+            this.selectionStart = this.selectionEnd = ss;
+        }
+    }).blur(function (e) {
+        var $this = $(this);
+
+        var val = $this.val();
+        var t = val.replace(/[^0-9]/g, '');
+        var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
+        var m = Math.max(0, Math.min(59, parseInt(t.substr(2)))) || 0;
+        var r = '';
+
+        if (!isNaN(h)) {
+            r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
+        }
+
+        $this.val(r);
+    });
+    //End math functions
+
+    //Time Block Mechanics
     $('.time-block .time-init').val('' + current_time + '');
     $('.time-block .time-last-init').attr("placeholder", "XX:XX " + am_pm + "");
 
@@ -25,10 +83,7 @@ $(document).ready(function () {
         '</div>';
 
     $(".add-time-block").on("click", "#time-block-button", function () {
-        var time_block_end = $('body').find('#time-last').val();
-        var time_block_start = $('body').find('#time-start').val();
-        console.log(time_block_start);
-        console.log(time_block_end);
+        var time_block_end = $('body').find('#time-last').last().val();
         $('.time-column').append(time_block);
         if (time_block_end != '') {
             $('.time-block #time-start').last().val('' + time_block_end + '');
@@ -37,56 +92,6 @@ $(document).ready(function () {
         }
     });
 
-    //Functions for automatically inserting colon and am/pm
-    $('input #time-start , #time-last').keydown(function (e) {
-    }).keyup(function (e) {
-        var $this = $(this);
 
-        if (e.ctrlKey || e.metaKey || e.which === 8 || e.which === 0 || (e.which >= 37 && e.which <= 40)) {
-            return true;
-        }
-
-        var selection_start = parseInt(this.selectionStart);
-
-        var val = $this.val();
-        var t = val.replace(/[^0-9]/g, '');
-        var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
-        var m = Math.max(0, Math.min(59, parseInt(t.substr(2))));
-
-        if (t.length < 3) {
-            m = '';
-        }
-
-        var r;
-
-        if (val.length === 2) {
-            r = String('0' + h).substr(String(h).length - 1) + ':';
-            selection_start++;
-        } else if (val.length >= 3 && val.length < 5) {
-            r = String('0' + h).substr(String(h).length - 1) + ':' + m;
-            selection_start++;
-        } else if (val.length === 5) {
-            r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
-        }
-
-        if (r && r !== $this.val()) {
-            $this.val(r);
-            this.selectionStart = this.selectionEnd = selection_start;
-        }
-    }).blur(function (e) {
-        var $this = $(this);
-
-        var v = $this.val();
-        var t = val.replace(/[^0-9]/g, '');
-        var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
-        var m = Math.max(0, Math.min(59, parseInt(t.substr(2)))) || 0;
-        var r = '';
-
-        if (!isNaN(h)) {
-            r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
-        }
-
-        $this.val(r);
-    });
 });
 
