bbc14ded3ffc88f19b9d3f2c9072d5de525d80d4
alex <alex@izoox.com>
2019-08-31 23:18:00 -0600

    Setting up schedule change functionality

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -74,68 +74,85 @@ $(document).ready(function () {
     });
     //end Time Block Mechanics
 
-    //Math functions for time and automatically inserting colon
-    $('#time-start , #time-last').keydown(function (e) {
-        if (e.ctrlKey || e.metaKey) {
-            return true;
+    //Time Column Mechanics
+    var time_column = '<div class="w-1/5" id="time-column">';
+
+    $("#schedule-change").on("click", function () {
+        $('#timesheet').append(time_column);
+        time_block_last = $('#time-column .time-block-last').val();
+        
+        if (time_block_last != '') {
+            $('#time-block #time-start').last().val('' + time_block_last + '');
+        } else {
+            $('#time-block #time-start').last().attr("placeholder", "XX:XX " + am_pm + "");
         }
+        $('#time-column #time-last').removeClass('time-block-last');
+        $('#time-column #time-last').last().addClass('time-block-last');
 
-        if (e.which >= 37 && e.which <= 40) {
-            return true;
-        }
+        //end Time Column Mechanics
 
-        if (e.which !== 8 && e.which !== 0 && e.key.match(/[^:0-9]/)) {
-            return false;
-        }
-    }).keyup(function (e) {
-        var $this = $(this);
+        //Math functions for time and automatically inserting colon
+        $('#time-start , #time-last').keydown(function (e) {
+            if (e.ctrlKey || e.metaKey) {
+                return true;
+            }
 
-        if (e.ctrlKey || e.metaKey || e.which === 8 || e.which === 0 || (e.which >= 37 && e.which <= 40)) {
-            return true;
-        }
+            if (e.which >= 37 && e.which <= 40) {
+                return true;
+            }
 
-        var ss = parseInt(this.selectionStart);
+            if (e.which !== 8 && e.which !== 0 && e.key.match(/[^:0-9]/)) {
+                return false;
+            }
+        }).keyup(function (e) {
+            var $this = $(this);
 
-        var val = $this.val();
-        var t = val.replace(/[^0-9]/g, '');
-        var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
-        var m = Math.max(0, Math.min(59, parseInt(t.substr(2))));
+            if (e.ctrlKey || e.metaKey || e.which === 8 || e.which === 0 || (e.which >= 37 && e.which <= 40)) {
+                return true;
+            }
 
-        if (t.length < 3) {
-            m = '';
-        }
+            var ss = parseInt(this.selectionStart);
 
-        var r;
+            var val = $this.val();
+            var t = val.replace(/[^0-9]/g, '');
+            var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
+            var m = Math.max(0, Math.min(59, parseInt(t.substr(2))));
 
-        if (val.length === 2) {
-            r = String('0' + h).substr(String(h).length - 1) + ':';
-            ss++;
-        } else if (val.length >= 3 && val.length < 5) {
-            r = String('0' + h).substr(String(h).length - 1) + ':' + m;
-            ss++;
-        } else if (val.length === 5) {
-            r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
-        }
+            if (t.length < 3) {
+                m = '';
+            }
 
-        if (r && r !== $this.val()) {
+            var r;
+
+            if (val.length === 2) {
+                r = String('0' + h).substr(String(h).length - 1) + ':';
+                ss++;
+            } else if (val.length >= 3 && val.length < 5) {
+                r = String('0' + h).substr(String(h).length - 1) + ':' + m;
+                ss++;
+            } else if (val.length === 5) {
+                r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
+            }
+
+            if (r && r !== $this.val()) {
+                $this.val(r);
+                this.selectionStart = this.selectionEnd = ss;
+            }
+        }).blur(function (e) {
+            var $this = $(this);
+
+            var val = $this.val();
+            var t = val.replace(/[^0-9]/g, '');
+            var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
+            var m = Math.max(0, Math.min(59, parseInt(t.substr(2)))) || 0;
+            var r = '';
+
+            if (!isNaN(h)) {
+                r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
+            }
+
             $this.val(r);
-            this.selectionStart = this.selectionEnd = ss;
-        }
-    }).blur(function (e) {
-        var $this = $(this);
-
-        var val = $this.val();
-        var t = val.replace(/[^0-9]/g, '');
-        var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
-        var m = Math.max(0, Math.min(59, parseInt(t.substr(2)))) || 0;
-        var r = '';
-
-        if (!isNaN(h)) {
-            r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
-        }
-
-        $this.val(r);
+        });
+        //End math functions  
     });
-    //End math functions  
-});
 
