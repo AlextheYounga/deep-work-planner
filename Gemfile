4778f75bda259f3567199969f08aaa70f82650fb
alex <alex@izoox.com>
2019-09-02 22:42:36 -0600

    Schedule change function is working and icons are loading

--- Gemfile
+++ Gemfile
@@ -30,7 +30,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'solargraph'
 gem 'jquery-rails'
-
+gem 'inline_svg'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -45,10 +45,8 @@ $('.contact-form').submit(function (e) {
 //End Autosaving Functions
 
 $(document).ready(function () {
-    //Time Block Mechanics
-    $('#time-block .time-init').val('' + current_time + '');
-    $('#time-block .time-last-init').attr("placeholder", "XX:XX " + am_pm + "");
-
+    //Variables
+    var time_column = '<div class="w-1/5 current-column" id="time-column"></div>';
     var time_block = '<div class="border-blue-400 border-l-2 my-12" id="time-block">' +
         '<div class="pl-2">' +
         '<input type="text" name="time-start" id="time-start" placeholder="" class="font-thin"></input>' +
@@ -61,98 +59,113 @@ $(document).ready(function () {
         '</div>' +
         '</div>';
 
+
+    //Time Block Init
+    $('#time-block .time-init').val('' + current_time + '');
+    $('#time-block .time-last-init').attr("placeholder", "XX:XX " + am_pm + "");
+
+    //Time Block Mechanics    
     $("#add-block-button").on("click", function () {
+        add_block_button = $('#add-block-button').detach();
         time_block_last = $('#time-column .time-block-last').val();
-        $('#time-column').append(time_block);
+
+        $('.current-column').append(time_block);
+        $('.current-column').append(add_block_button);
+
         if (time_block_last != '') {
             $('#time-block #time-start').last().val('' + time_block_last + '');
         } else {
             $('#time-block #time-start').last().attr("placeholder", "XX:XX " + am_pm + "");
         }
+
         $('#time-column #time-last').removeClass('time-block-last');
         $('#time-column #time-last').last().addClass('time-block-last');
     });
     //end Time Block Mechanics
 
-    //Time Column Mechanics
-    var time_column = '<div class="w-1/5" id="time-column">';
 
-    $("#schedule-change").on("click", function () {
-        $('#timesheet').append(time_column);
+    //Time Column Mechanics
+    $("#toolbar #schedule-change").on("click", function () {
+        add_block_button = $('#add-block-button').detach();
         time_block_last = $('#time-column .time-block-last').val();
-        
-        if (time_block_last != '') {
-            $('#time-block #time-start').last().val('' + time_block_last + '');
-        } else {
-            $('#time-block #time-start').last().attr("placeholder", "XX:XX " + am_pm + "");
-        }
+
+        $('#timesheet #time-column').removeClass('current-column');
+        $('#timesheet').append(time_column);
+        $('.current-column').append(time_block);
+        $('.current-column').append(add_block_button);
+
+        $('#time-block #time-start').last().val('' + current_time + '');
+        $('#time-block #time-last').last().attr("placeholder", "XX:XX " + am_pm + "");;
+
         $('#time-column #time-last').removeClass('time-block-last');
         $('#time-column #time-last').last().addClass('time-block-last');
 
-        //end Time Column Mechanics
-
-        //Math functions for time and automatically inserting colon
-        $('#time-start , #time-last').keydown(function (e) {
-            if (e.ctrlKey || e.metaKey) {
-                return true;
-            }
-
-            if (e.which >= 37 && e.which <= 40) {
-                return true;
-            }
-
-            if (e.which !== 8 && e.which !== 0 && e.key.match(/[^:0-9]/)) {
-                return false;
-            }
-        }).keyup(function (e) {
-            var $this = $(this);
-
-            if (e.ctrlKey || e.metaKey || e.which === 8 || e.which === 0 || (e.which >= 37 && e.which <= 40)) {
-                return true;
-            }
-
-            var ss = parseInt(this.selectionStart);
-
-            var val = $this.val();
-            var t = val.replace(/[^0-9]/g, '');
-            var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
-            var m = Math.max(0, Math.min(59, parseInt(t.substr(2))));
-
-            if (t.length < 3) {
-                m = '';
-            }
-
-            var r;
-
-            if (val.length === 2) {
-                r = String('0' + h).substr(String(h).length - 1) + ':';
-                ss++;
-            } else if (val.length >= 3 && val.length < 5) {
-                r = String('0' + h).substr(String(h).length - 1) + ':' + m;
-                ss++;
-            } else if (val.length === 5) {
-                r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
-            }
-
-            if (r && r !== $this.val()) {
-                $this.val(r);
-                this.selectionStart = this.selectionEnd = ss;
-            }
-        }).blur(function (e) {
-            var $this = $(this);
-
-            var val = $this.val();
-            var t = val.replace(/[^0-9]/g, '');
-            var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
-            var m = Math.max(0, Math.min(59, parseInt(t.substr(2)))) || 0;
-            var r = '';
-
-            if (!isNaN(h)) {
-                r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
-            }
-
-            $this.val(r);
-        });
-        //End math functions  
     });
+
+    //end Time Column Mechanics
+
+    //Math functions for time and automatically inserting colon
+    $('#time-start , #time-last').keydown(function (e) {
+        if (e.ctrlKey || e.metaKey) {
+            return true;
+        }
+
+        if (e.which >= 37 && e.which <= 40) {
+            return true;
+        }
+
+        if (e.which !== 8 && e.which !== 0 && e.key.match(/[^:0-9]/)) {
+            return false;
+        }
+    }).keyup(function (e) {
+        var $this = $(this);
+
+        if (e.ctrlKey || e.metaKey || e.which === 8 || e.which === 0 || (e.which >= 37 && e.which <= 40)) {
+            return true;
+        }
+
+        var ss = parseInt(this.selectionStart);
+
+        var val = $this.val();
+        var t = val.replace(/[^0-9]/g, '');
+        var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
+        var m = Math.max(0, Math.min(59, parseInt(t.substr(2))));
+
+        if (t.length < 3) {
+            m = '';
+        }
+
+        var r;
+
+        if (val.length === 2) {
+            r = String('0' + h).substr(String(h).length - 1) + ':';
+            ss++;
+        } else if (val.length >= 3 && val.length < 5) {
+            r = String('0' + h).substr(String(h).length - 1) + ':' + m;
+            ss++;
+        } else if (val.length === 5) {
+            r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
+        }
+
+        if (r && r !== $this.val()) {
+            $this.val(r);
+            this.selectionStart = this.selectionEnd = ss;
+        }
+    }).blur(function (e) {
+        var $this = $(this);
+
+        var val = $this.val();
+        var t = val.replace(/[^0-9]/g, '');
+        var h = Math.max(0, Math.min(23, parseInt(t.substr(0, 2))));
+        var m = Math.max(0, Math.min(59, parseInt(t.substr(2)))) || 0;
+        var r = '';
+
+        if (!isNaN(h)) {
+            r = String('0' + h).substr(String(h).length - 1) + ':' + String('0' + m).substr(String(m).length - 1);
+        }
+
+        $this.val(r);
+    });
+    //End math functions  
+});
 
